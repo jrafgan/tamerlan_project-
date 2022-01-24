f1f1befb986a1cf2c925e91e6e6ed8e912ba5704
Maksat Duishenaliev <maksatovy@gmail.com>
2022-01-24 15:05:58 +0600

    paid and paidDays fields added

--- back_end/models/Ad.js
+++ back_end/models/Ad.js
@@ -2,46 +2,54 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const AdSchema = new Schema({
-        imageUrls: {
-            type: Array
-        },
-        adTitle: {
-            type: String
-        },
-        price: {
-            type: String
-        },
-        date: {
-            type: Date,
-            default: Date.now(),
-        },
-        phone: {
-            type: String
-        },
-        user_id: {
-            required: true,
-            type: String
-        },
-        city: {
-            type: String
-        },
-        shortDescription: {
-            type: String
-        },
-        fullDescription: {
-            type: String
-        },
-        selectedCategory: {
-            type: String
-        },
-        selectedSubCategory: {
-            type: String
-        },
-        moderated: {
-            type: Boolean,
-            default: false
-        }
-    });
+    imageUrls: {
+        type: Array
+    },
+    adTitle: {
+        type: String
+    },
+    price: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now(),
+    },
+    phone: {
+        type: String
+    },
+    user_id: {
+        required: true,
+        type: String
+    },
+    city: {
+        type: String
+    },
+    shortDescription: {
+        type: String
+    },
+    fullDescription: {
+        type: String
+    },
+    selectedCategory: {
+        type: String
+    },
+    selectedSubCategory: {
+        type: String
+    },
+    moderated: {
+        type: Boolean,
+        default: false
+    },
+    paid: {
+        type: Boolean,
+        default: false
+    },
+    paidDays: {
+        type: Number,
+        default: 0
+    },
+});
 
 const Ad = mongoose.model('Ad', AdSchema);
 
